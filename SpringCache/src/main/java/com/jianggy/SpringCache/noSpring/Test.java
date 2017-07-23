715d3c1af3bd6808c21ca43265350858b0edf249
jianggy <jguangyou_2016@163.com>
2017-07-23 22:36:42 +0800

    v2.0版本

--- SpringCache/src/main/java/com/jianggy/SpringCache/noSpring/Test.java
+++ SpringCache/src/main/java/com/jianggy/SpringCache/noSpring/Test.java
@@ -11,5 +11,7 @@ public class Test {
 		service.getUserById("111");
 		System.out.println("第三次获取User");
 		service.getUserById("111");
+		System.out.println("版本v2.0");
+		service.getUserById("111");
 	}
 }
